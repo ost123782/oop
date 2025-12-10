b6ccf4d17bc2d9d8ae9510584930d50c2004190c
ost123 <83078347+ost123782@users.noreply.github.com>
2025-12-10 11:10:01 +0200

    Update main.cpp

--- lab_9-10/main.cpp
+++ lab_9-10/main.cpp
@@ -9,12 +9,12 @@ using namespace std;
 int main() {
     vector<unique_ptr<Circuit>> circuits;
 
-    circuits.push_back(make_unique<Circuit>(100, "Test IC"));
-    circuits.push_back(make_unique<Ram>(100, "RAM", 8));
-    circuits.push_back(make_unique<LogicIc>(100, "AND IC", "AND"));
+    circuits.push_back(make_unique<Circuit>(100, "Base Circuit"));
+    circuits.push_back(make_unique<Ram>(2000000, "Kingston RAM", 16));
+    circuits.push_back(make_unique<LogicIc>(5000, "SN7400", "NAND Logic"));
 
-    for (auto &c : circuits) {
-        c->showInfo();
+    for (const auto& circuit : circuits) {
+        circuit->showInfo();
     }
 
     return 0;
